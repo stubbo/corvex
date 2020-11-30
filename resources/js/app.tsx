3fcd9b6a5ad3a4fc0171e353922467d95c197c34
Subbo <georgestubbs24@gmail.com>
2020-11-30 23:40:16 +0000

    Added access to app services when in dev build

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import * as Sentry from '@sentry/react';
 import App from './Components/App';
 
+import AuthService from 'Services/AuthService';
+import RouteService from 'Services/RouteService';
+
 if (process.env.NODE_ENV === 'production') {
   Sentry.init({
     dsn: process.env.MIX_SENTRY_LARAVEL_DSN,
   });
+} else {
+  /** @ts-ignore */
+  window.appServices = {
+    auth: AuthService,
+    router: RouteService,
+  };
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
