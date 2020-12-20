697a44fed08e81f72e463afd4411224efbd369f2
Subbo <georgestubbs24@gmail.com>
2020-12-20 01:54:01 +0000

    Fixed production building and css purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: [],
+  purge: [
+    './resources/js/**.*.tsx'
+  ],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
